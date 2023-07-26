5b020950c306e42e797f3bcf6a1e7cf8920fec51
JuanFrancisco <juanarmijoreyes@gmail.com>
2023-07-26 18:16:50 -0400

    Tablas funcionando adecuadamente por llaves foraneas

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-    class Pet < ApplicationRecord
+
         belongs_to :user # un comentario pertenece a un solo usuario 
         has_many :images, as: :imageable # un comentario puede tener muchos usuarios, llave polimorfica
+
 end
